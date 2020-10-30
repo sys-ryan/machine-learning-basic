874b75752c4c1933dea340ca8adafb5faf5b4776
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-10-30 20:04:23 +0900

    Create 03_MultipleLinearRegression.py
    03 Multiple Linear Regression

--- 02. Simple Linear Regression/02_SimpleLinearRegression.py
+++ 02. Simple Linear Regression/02_SimpleLinearRegression.py
@@ -7,7 +7,7 @@ import pandas as pd
 dataset = pd.read_csv('Salary_Data.csv')
 x = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
-
+  
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
