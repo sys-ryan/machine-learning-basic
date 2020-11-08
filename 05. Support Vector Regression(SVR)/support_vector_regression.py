d6d43d6f944618df6d35a08e5102186e7016d6f6
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-11-09 00:41:03 +0900

    05 Support Vector Regression (SVR)
    - support_vector_regression.py

---
+++ 05. Support Vector Regression(SVR)/support_vector_regression.py
@@ -0,0 +1,60 @@
+# Support Vector Regression (SVR)
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# Importing the dataset 
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+y = y.reshape(len(y), 1) # for 
+print(y)
+
+
+# Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x = sc_x.fit_transform(x)
+
+sc_y = StandardScaler()
+y = sc_y.fit_transform(y)
+
+print(x)
+print(y)
+
+
+# Training the SVR model on the whole dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')   # there are many kinds of kernels
+regressor.fit(x, y)
+
+
+# Predicting a new result
+prediction = sc_y.inverse_transform(regressor.predict(sc_x.transform([[6.5]])))
+print(prediction)
+
+
+# Visualising the SVR results
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color='red')  # original scales of x and y 
+plt.plot(sc_x.inverse_transform(x), sc_y.inverse_transform(regressor.predict(x)), color='blue')
+plt.title('Truth or Bluff (Support Vector Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+x_grid = np.arange(min(sc_x.inverse_transform(x)), max(sc_x.inverse_transform(x)), 0.1)
+x_grid = x_grid.reshape((len(x_grid), 1))
+
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color='red')  # original scales of x and y 
+plt.plot(x_grid, sc_y.inverse_transform(regressor.predict(sc_x.transform(x_grid))), color='blue') # x_grid is in original scales
+plt.title('Truth or Bluff (Support Vector Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
